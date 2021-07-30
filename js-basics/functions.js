28fcf2a2117476e8c4c010d27bf8c284486ca09f
Tyler Merritt <tjmerritt5@gmail.com>
2021-07-30 09:48:25 -0600

    Initial commit

---
+++ js-basics/functions.js
@@ -0,0 +1,38 @@
+
+//////////////////PROBLEM 1////////////////////
+/*
+  Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. 
+  The function should return the sum of the two parameters.
+*/
+
+//CODE HERE
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+  Create a function called 'greaterThanFive' that takes in a single parameter called 'number'. 
+  Check to see if 'number' is greater than 5.
+  If it is, return a true boolean. 
+  If it's not, return a false boolean.
+*/
+
+//CODE HERE
+
+//////////////////PROBLEM 3////////////////////
+/*
+  Create a function called 'findZWords' that takes in one parameter, 'word'. 
+  Your function should check to see if the name begins with the letter "Z"
+  If the name does begin with Z, return the string: 'the name begins with Z'. 
+  If the name does not begin with Z, return the string: 'the name does not begin with Z'.
+*/
+
+//CODE HERE
+
+//////////////////PROBLEM 4////////////////////
+/*
+  Create a function called 'iLove' that takes in two string parameters, 'name' and 'love'. 
+  Have the function take the two parameters and return a string that says 
+  "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Joseph loves music"
+*/
+
+//CODE HERE
