7d4666ad510c8c3d52e89eef453f15e6bf6dbdf2
Tyler Merritt <tjmerritt5@gmail.com>
2021-07-30 11:54:03 -0600

    made the program

---
+++ js-basics/password.js
@@ -0,0 +1,22 @@
+//got some code from the calculator assignment
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log(`Welcome to the validator tool`)
+reader.question("Enter password to validate:", function(input){
+    token = input.split('')
+    if (token.length === 10){
+        console.log(`Your password passes validation`)
+    } else{
+        console.log(`Your password will not work`)
+    }
+
+    
+
+    reader.close()
+
+});
